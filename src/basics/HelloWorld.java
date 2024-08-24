24367c99f3c0e480fe20eb376f0c53ffd17d2067
swarnambigamk <swarnambiga@gmail.com>
2024-08-24 16:52:55 +0530

    first commit

---
+++ src/basics/HelloWorld.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		/*
+		 * float a,b,c; a=7.4f; b=5.3f; c=a+b;
+		 */
+		String name;
+		name="Muthukumaran";
+		//System.out.println("Sum="+c);
+		System.out.println("Name="+name);
+		
+
+	}
+
+}
